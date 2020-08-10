58fad716ba92da638ac37c9f1f2cc663a58a4be3
Sveta Krivosheeva <sveta@effective.band>
2020-08-10 15:33:49 +0600

    fix import path

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lastochki/views/home_page.dart';
+import 'package:lastochki/views/screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
