38190f520af556b619370e4d71f7677d16cae65f
Sveta Krivosheeva <ly4chij.sveta@gmail.com>
2020-08-24 13:07:51 +0600

    add home screen && resources

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:lastochki/views/screens/home_page.dart';
 import 'package:lastochki/views/screens/onboarding_page.dart';
 
+import 'models/entities/Name.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isFirstStart = prefs.getBool('isFirstStart') ?? true;
+  // TODO set language on single place
+  String languageCode = prefs.getString('languageCode');
+  if (languageCode != null) {
+    Name.curLocale = Locale(languageCode);
+  }
   prefs.setBool('isFirstStart', false);
   runApp(App(isFirstStart: isFirstStart));
 }
@@ -28,7 +36,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: isFirstStart ? OnboardingPage() : HomePage(),
+      home: !isFirstStart ? OnboardingPage() : HomePage(),
     );
   }
 }
