62e02a1e77ec1961b1b1fc5e081728e4848b5fde
Pavel <p97p@ya.ru>
2021-08-22 17:13:50 +0300

    translations

--- lib/views/translation.dart
+++ lib/views/translation.dart
@@ -151,13 +151,31 @@ const Name loading = Name(
 const Name chapterPreparing =
     Name(ru: 'Подготовка главы...', kg: 'Бөлүмдү даярдоо...');
 
-const Name gameEndTitle = Name(ru: "Конец истории", kg: "Окуянын аягы");
-const Name restartGame = Name(ru: "Играть заново", kg: "Кайрадан ойнотуу");
+const Name gameEndTitle = Name(
+  ru: "Конец истории",
+  kg: "Окуянын аягы",
+);
+const Name restartGame = Name(
+  ru: "Играть заново",
+  kg: "Кайрадан ойнотуу",
+);
 const Name restartGameText = Name(
-    ru: "Можешь сыграть ещё раз с новыми выборами! ",
-    kg: "Жаны тандоо менен оюнду кайра өтүү!");
-const Name updateTitle = Name(ru: "Глава \$number обновлена", kg: "");
-const Name updateDescription =
-    Name(ru: "Мы сделали главу ещё лучше, скорее возвращайся к игре!", kg: "");
-const Name download = Name(ru: "Скачать", kg: "");
-const Name back = Name(ru: "Назад", kg: "");
+  ru: "Можешь сыграть ещё раз с новыми выборами! ",
+  kg: "Жаны тандоо менен оюнду кайра өтүү!",
+);
+const Name updateTitle = Name(
+  ru: "Глава \$number обновлена",
+  kg: "\$number-бөлүм жаңыланды",
+);
+const Name updateDescription = Name(
+  ru: "Мы сделали главу ещё лучше, скорее возвращайся к игре!",
+  kg: "Биз оюнду дагы кызыктуураак кылдык, андыктан тезирээк оюнга кайыт кел!",
+);
+const Name download = Name(
+  ru: "Скачать",
+  kg: "Жүктөө",
+);
+const Name back = Name(
+  ru: "Назад",
+  kg: "Артка",
+);
