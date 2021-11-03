086c14256f246c86952135b3c9cf9a3caa04862d
AdmiralMealty <admiralmealty@gmail.com>
2021-11-03 11:24:58 +0300

    app open time

--- lib/services/analytics_service.dart
+++ lib/services/analytics_service.dart
@@ -5,6 +5,7 @@ abstract class AnalyticsService {
   void log(String message);
 
   get observer;
+
   void setCurrentScreen({String screenName});
 }
 
@@ -13,9 +14,23 @@ class FirebaseAnalyticsService implements AnalyticsService {
   FirebaseAnalyticsObserver _observer;
 
   FirebaseAnalyticsService() {
-
     _observer = FirebaseAnalyticsObserver(analytics: _analytics);
-    _analytics.logAppOpen();
+    final now = DateTime.now().hour;
+    if (now >= 0 && now < 3)
+      log('App open 0:00-3:00');
+    else if (now >= 3 && now < 6)
+      log('App open 3:00-6:00');
+    else if (now >= 6 && now < 9)
+      log('App open 6:00-9:00');
+    else if (now >= 9 && now < 12)
+      log('App open 9:00-12:00');
+    else if (now >= 12 && now < 15)
+      log('App open 12:00-15:00');
+    else if (now >= 15 && now < 18)
+      log('App open 15:00-18:00');
+    else if (now >= 18 && now < 21)
+      log('App open 18:00-21:00');
+    else if (now >= 21) log('App open 21:00-0:00');
   }
 
   void log(String message) {
@@ -24,7 +39,7 @@ class FirebaseAnalyticsService implements AnalyticsService {
 
   get observer => _observer;
 
-  Future<void> setCurrentScreen({String screenName}) async{
+  Future<void> setCurrentScreen({String screenName}) async {
     _analytics.setCurrentScreen(screenName: screenName);
   }
 }
