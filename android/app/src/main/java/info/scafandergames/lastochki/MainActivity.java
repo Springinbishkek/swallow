af6cc4f66caf030273a3a646752c4dc8376fca0e
Pavel <p97p@ya.ru>
2022-02-02 20:36:28 +0300

    Revert "facebook sdk for android"
    This reverts commit b88b2f5c

--- android/app/src/main/java/info/scafandergames/lastochki/MainActivity.java
+++ android/app/src/main/java/info/scafandergames/lastochki/MainActivity.java
@@ -1,15 +1,6 @@
 package info.scafandergames.lastochki;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FbEvents.initialize(this);
-    }
 }
